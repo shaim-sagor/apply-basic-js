14c4e2f3866297dc9fb915f8c06edbd4a263ac90
shaim-sagor <shaim.sagor.11@gmail.com>
2022-11-13 22:29:10 +0600

    factorial-while

---
+++ factorial-while.js
@@ -0,0 +1,26 @@
+// function getFactorial(number) {
+//     let factorial = 1;
+//     let i = 1;
+//     while (i <= number) {
+//         factorial = factorial * i;
+//         i++;
+//     }
+//     return factorial;
+// }
+
+function getFactorial(number) {
+    let factorial = 1;
+    let i = number;
+    while (i >= 1) {
+        factorial = factorial * i;
+        i--;
+    }
+    return factorial;
+}
+
+
+
+
+var given1 = 5;
+var result1 = getFactorial(given1);
+console.log(result1);
